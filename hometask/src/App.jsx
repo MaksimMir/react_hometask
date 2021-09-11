0b9ddec90a52e85d410edb87ac63a7560c35f596
MaxM <mak-miroshnichen@yandex.ru>
2021-09-11 15:36:12 +0300

    task2

--- hometask/src/App.jsx
+++ hometask/src/App.jsx
@@ -1,14 +1,52 @@
+import { useState, useEffect } from "react";
+import Dialog from "./components/Dialog/Dialog";
 import Message from "./components/Message/Message";
+import MessageScreen from "./components/MessageScreen/MessageScreen";
 
 
-const text = 'Hello world';
+const App = () => {
+    const [ messageList, setMessageList ] = useState([])
+    const [ isVisible, setIsVisible] = useState(false);
+    const [ messageName, setMessageName ] = useState('')
 
-function App() {
-  return (
-    <>
-      <Message message={text} />
-    </>
-  );
+
+    const changeList = (text, name) => {
+
+      const newMessage = {
+        name: name,
+        text: text,
+        id: Math.random()
+      }
+      
+      setMessageList([...messageList, newMessage])
+      
+    }
+
+    const closeDialogWindow = () => {
+      setIsVisible(false)
+    }
+
+    useEffect(() => {
+      if (!messageList.length) return;
+       
+      setMessageName(messageList[messageList.length - 1].name)
+
+      setTimeout(() => {
+        setIsVisible(true)
+      }, 1500)
+
+      return () => {
+        setIsVisible(false)        
+      }
+    }, [messageList])
+
+    return (
+      <>
+        <Message changeList={changeList} />
+        {isVisible && <Dialog name={messageName} closeDialogWindow={closeDialogWindow} />}
+        <MessageScreen messageList={messageList} />
+      </>
+    );
 }
 
 export default App;
